d08dca4be2e4cda0668e52bbcbe15baa2bdd7323
danielguitara <danielguitara4@gmail.com>
2025-04-25 16:58:22 +0700

    update design and upload assets

--- src/App.js
+++ src/App.js
@@ -1,35 +1,73 @@
-import daniel from './assets/daniel.svg'
-import linkedin from './assets/linkedin.png'
-import instagram from './assets/instagram.png'
-import twitter from './assets/twitter.png'
-import youtube from './assets/youtube.png'
-import tiktok from './assets/tiktok.png'
-import apple from './assets/apple.png'
+import daniel from './assets/daniel.jpg'
+import linkedin from './assets/linkedin.svg'
+import instagram from './assets/instagram.svg'
+import twitter from './assets/twitter.svg'
+import tiktok from './assets/tiktok.svg'
+import link from './assets/link.svg'
 import './App.css';
 
 function App() {
   return (
     <div className="container">
-      <section className="header">DG</section>
-      <section className="content">
-        <div className="profile">
-          <div className="profile-picture"><img src={daniel} width="125px" height="125px" alt="Daniel"/></div>
-          <div className="profile-desc">
-            <div className="profile-desc__title">I'm Daniel Guitara</div>
-            <div className="profile-desc__sub">a software developer, designer and creator.</div>
-            <div className="profile-desc__sub">business enquires/collaboration: danielguitara4@gmail.com</div>
+      <div></div>
+      <div>
+        <section className="content">
+          <div className="block">
+            <div className="block-picture"><img src={daniel} width="100px" height="100px" alt="Daniel"/></div>
+            <div className="block-desc">
+              <div className="block-desc__title">I'm Daniel - a designer and creator.</div>
+              <div className="block-desc__sub">I started as a software developer making website in several companies, but now interested in design and do anything related. Design app, build website, running e-commerce stores and I also make Youtube videos. I'm from West Java, Indonesia.</div>
+            </div>
           </div>
-        </div>
-        <div className="links">
-          <a href="https://www.linkedin.com/in/danielguitara" className="link"><img src={linkedin} width="30px" alt="Linkedin"/><span>My Linkedin</span></a>
-          <a href="https://www.instagram.com/danielguitara" className="link"><img src={instagram} width="30px" alt="Instagram"/><span>My Instagram</span></a>
-          <a href="https://www.tiktok.com/@danielguitara" className="link"><img src={tiktok} width="30px" alt="Tiktok"/><span>My Tiktok</span></a>
-          <a href="https://music.apple.com/profile/danielguitara" className="link"><img src={apple} height="30px" alt="Apple"/><span>My Apple Music</span></a>
-          <a href="https://www.twitter.com/danielguitara" className="link"><img src={twitter} width="30px" alt="Twitter"/><span>My Twitter</span></a>
-          <a href="https://www.youtube.com/@danielguitara" className="link"><img src={youtube} width="30px" alt="Youtube"/><span>My Youtube</span></a>
-        </div>
-      </section>
-      <section className="footer">Copyright © {(new Date().getFullYear())} Daniel Guitara. All rights reserved.</section>
+        </section>
+        <section className="content">
+          <div className="block">
+            <div className="block-title">Creating</div>
+          </div>
+          <div className="block half">
+            <div className="block-list">
+              <div className="block-list__title">
+                <a href="https://daniels-portofolio-3e259e.webflow.io" className="link-create">Portfolio<img src={link} width="12px" alt="Lightroom Presets"/></a>
+              </div>
+              <div className="block-list__sub">Design and build beautiful websites for businesses around the globe.</div>
+            </div>
+            {/* <div className="block-list">
+              <div className="block-list__title">
+                <a href="https://store.danielguitara.com/collection/all" className="link-create">Lightroom Presets<img src={link} width="12px" alt="Lightroom Presets"/></a>
+              </div>
+              <div className="block-list__sub">A series of Lightroom presets custom made from years of editing photos.</div>
+            </div> */}
+            <div className="block-list">
+              <div className="block-list__title">
+                <a href="https://store.danielguitara.com/collection/all" className="link-create">Wallpapers<img src={link} width="12px" alt="Wallpapers"/></a>
+              </div>
+              <div className="block-list__sub">Designing and making premium wallpapers for desktops and mobile.</div>
+            </div>
+            <div className="block-list">
+              <div className="block-list__title">
+                <a href="https://www.youtube.com/@danielguitara" className="link-create">Youtube<img src={link} width="12px" alt="Youtube"/></a>
+              </div>
+              <div className="block-list__sub">Uploading videos covering music, tech, design and lifestyle.</div>
+            </div>
+          </div>
+        </section>
+        <section className="content">
+          <div className="block">
+            <div className="block-title">Elsewhere</div>
+            <div className="block-links">
+              <a href="https://www.linkedin.com/in/danielguitara" className="link"><img src={linkedin} width="18px" alt="Linkedin"/><span>danielguitara</span></a>
+              <a href="https://www.instagram.com/danielguitara" className="link"><img src={instagram} width="18px" alt="Instagram"/><span>danielguitara</span></a>
+              <a href="https://www.twitter.com/danielguitara" className="link"><img src={twitter} width="18px" alt="Twitter"/><span>danielguitara</span></a>
+              <a href="https://www.tiktok.com/@danielguitara" className="link"><img src={tiktok} width="18px" alt="Tiktok"/><span>danielguitara</span></a>
+            </div>
+          </div>
+        </section>
+      </div>
+      <div className="footer">
+          <div className="footer-desc">
+            <div className="block">© 2025 danielguitara. All rights reserved.</div>
+          </div>
+      </div>
     </div>
   );
 }
